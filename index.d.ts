a83508082c1bcf2a1e2b2f69d8fa537d4509b1cf
Jagger <linjie.gao@tendcloud.com>
2019-03-22 11:47:38 +0800

    chore: optimize project.

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-import Pageable from './src/Pageable'
-
-export default Pageable
-export { default as PagingSource } from './src/PagingSource'
---
+++ index.js
@@ -0,0 +1,4 @@
+import Pageable from './typings/Pageable'
+
+export default Pageable
+export { default as PagingSource } from './typings/PagingSource'
