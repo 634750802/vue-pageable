35b9ae17bc5716a0de6f1f1226171cf43022c37d
Jagger <linjie.gao@tendcloud.com>
2019-03-22 15:34:16 +0800

    docs: fix docs config.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Vue Pageable',
   description: 'Vue pageable is aimed to support common paging logic.',
-  base: '/vue-pageable',
+  base: '/vue-pageable/',
   themeConfig: {
     search: false,
     nav: [
